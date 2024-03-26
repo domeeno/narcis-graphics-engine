2d2c2cc1c2f06b4f1fb864ac277526f0483f2f96
domeeno <dominicflocea.dev@gmail.com>
2024-03-26 20:19:20 +0200

    add glfw window with esc to close

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,28 @@
+#include "../include/glad/glad.h"
 #include <GL/gl.h>
-#include <cstdlib>
 #include <GLFW/glfw3.h>
+#include <cstdlib>
+#include <iostream>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
 
 int main(int argc, char **argv) {
   GLFWwindow *window;
 
-  if(!glfwInit())
+  if (!glfwInit())
     return EXIT_FAILURE;
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 
@@ -18,8 +33,19 @@ int main(int argc, char **argv) {
 
   glfwMakeContextCurrent(window);
 
-  while(!glfwWindowShouldClose(window)) {
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cerr << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, 640, 480);
+
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
+
+    processInput(window);
 
     glfwSwapBuffers(window);
 
