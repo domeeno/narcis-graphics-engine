87c5c17b86647d07b756ee79b454fa4ff7f46944
domeeno <dominicflocea.dev@gmail.com>
2024-03-27 21:41:56 +0200

    draw two triangles. freeing memory buffer/shader program

--- src/main.cpp
+++ src/main.cpp
@@ -13,8 +13,7 @@ const char *WINDOW_NAME = "Hello";
 int main(int argc, char **argv) {
 
   GLFWwindow *window = initGlfwWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
-                                     3, 3, GLFW_OPENGL_CORE_PROFILE);
-
+                                      3, 3, GLFW_OPENGL_CORE_PROFILE);
 
   unsigned int shaderProgram = init_shader_program(
       init_vertex_shader(),  // return vertex shader program id
@@ -24,16 +23,34 @@ int main(int argc, char **argv) {
   float vertices[] = {
       -0.1f, -0.2f, 0.0f, // bottom left
       0.1f,  -0.2f, 0.0f, // bottom right
-      0.0f,  0.15f, 0.0f  // top
+      0.0f,  0.15f, 0.0f, // top
+
+      -0.4f, -0.2f, 0.0f, // bottom left
+      -0.2f, -0.2f, 0.0f, // bottom right
+      -0.3f, 0.15f, 0.0f, // top
+
   };
 
-  unsigned int VAO, VBO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
-  glBindVertexArray(VAO);
+  float triangleRightVertices[] = {
+      0.2f, -0.2f, 0.0f, // bottom left
+      0.4f, -0.2f, 0.0f, // bottom right
+      0.3f, 0.15f, 0.0f  // top
+  };
 
+  // create a memory on the gpu where vertex data will be stored
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+
+  // vertex buffer array object binding
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Core OpenGL requires that a VAO should be used, to know what to do with
+  // vertex inputs from VBO
+  unsigned int VAO;
+  // vertex array object binding
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
 
   // tell GL to interpret vertex data as
   // |       vertex 1     ||       vertex2      ||      vertex3       |
@@ -41,9 +58,11 @@ int main(int argc, char **argv) {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
 
+  // unbind
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
 
+  // wireframe mode
   glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   while (!glfwWindowShouldClose(window)) {
@@ -53,11 +72,16 @@ int main(int argc, char **argv) {
 
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, 9);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+
+  glDeleteProgram(shaderProgram);
 
   glfwTerminate();
 
