e8a5f8b4eaf94b298085030078d77f48984ae617
domeeno <dominicflocea.dev@gmail.com>
2024-03-31 01:47:27 +0200

    add escape keys list.

--- src/game.cpp
+++ src/game.cpp
@@ -1,13 +1,17 @@
 #include "../include/glad/glad.h"
 #include "game.h"
 #include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
+static int escapeKeys[] = {GLFW_KEY_ESCAPE, GLFW_KEY_Q};
 
 void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
+  for (int key : escapeKeys) {
+    if (glfwGetKey(window, key) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, true);
+    }
   }
 }
-
 
 float vertices[] = {
     -0.5f, -0.5f, 0.0f, //
