1f4c854ba49c4d3efebf6848583d8f3e1d1071fb
domeeno <dominicflocea.dev@gmail.com>
2024-04-07 11:37:39 +0300

    init nge migration

---
+++ src/nge/Engine/AppEngine.hpp
@@ -0,0 +1,8 @@
+#ifndef ENGINE_APP_ENGINE_H_
+#define ENGINE_APP_ENGINE_H_
+
+namespace nge {
+class AppEngine {};
+} // namespace nge
+
+#endif // ENGINE_APP_ENGINE_H_
---
+++ src/nge/Nge.hpp
@@ -0,0 +1,10 @@
+#ifndef NGE_H_
+#define NGE_H_
+
+/**
+ *
+ * Narcis Graphics Engine header
+ *
+ */
+
+#endif //NGE_H_
